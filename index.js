6e5f2a8489f808c4c9c6338ad0bec97220ace182
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-20 16:02:04 +0300

    Need to debug - potentially API endpoint is incorrect. Resume vid at 24:55 once fixed to add formatting

--- index.js
+++ index.js
@@ -1,4 +1,86 @@
 // Import stylesheets
 import './style.css';
+const fromSelect = document.querySelector('[name="from_currency"]');
+const fromInput = document.querySelector('[name="from_amount"]');
+const toSelect = document.querySelector('[name="to_currency"]');
+const toEl = document.querySelector('.to_amount');
+const form = document.querySelector('.app form');
+const endpoint = 'https://api.exchangerateapi.io/latest';
+const ratesByBase = {};
 
-// Write Javascript code!
+
+  // Write Javascript code!
+const currencies = {
+  USD: 'United States Dollar',
+  AUD: 'Australian Dollar',
+  BGN: 'Bulgarian Lev',
+  BRL: 'Brazilian Real',
+  CAD: 'Canadian Dollar',
+  CHF: 'Swiss Franc',
+  CNY: 'Chinese Yuan',
+  CZK: 'Czech Republic Koruna',
+  DKK: 'Danish Krone',
+  GBP: 'British Pound Sterling',
+  HKD: 'Hong Kong Dollar',
+  HRK: 'Croatian Kuna',
+  HUF: 'Hungarian Forint',
+  IDR: 'Indonesian Rupiah',
+  ILS: 'Israeli New Sheqel',
+  INR: 'Indian Rupee',
+  JPY: 'Japanese Yen',
+  KRW: 'South Korean Won',
+  MXN: 'Mexican Peso',
+  MYR: 'Malaysian Ringgit',
+  NOK: 'Norwegian Krone',
+  NZD: 'New Zealand Dollar',
+  PHP: 'Philippine Peso',
+  PLN: 'Polish Zloty',
+  RON: 'Romanian Leu',
+  RUB: 'Russian Ruble',
+  SEK: 'Swedish Krona',
+  SGD: 'Singapore Dollar',
+  THB: 'Thai Baht',
+  TRY: 'Turkish Lira',
+  ZAR: 'South African Rand',
+  EUR: 'Euro',
+};
+
+function generateOptions(options){
+  return Object.entries(options)
+  .map(
+    ([currencyCode, currencyName]) => 
+    `<option value="${currencyCode}">${currencyCode} - ${currencyName}</option>`  
+  )
+  .join('');
+};
+
+async function fetchRates(base = 'USD'){
+  const res = await fetch(`${endpoint}?base=${base}`);
+  const rates = await res.json();
+  return rates; 
+}
+
+async function convert(amount, from, to){
+  // CHECK IF RATES ARE CONVERTABLE
+  if(!ratesByBase[from]) {
+    console.log('fail');
+    const rates = await fetchRates(from);
+    ratesByBase[from] = rates;
+  }
+  //convert amount passed into form
+  const rate = ratesByBase[from].rates[to];
+  const convertedAmount = rate * amount; 
+  return convertedAmount;
+}
+
+async function handleInput(e){
+  const rawAmount = await convert(fromInput.value, fromSelect.value, toSelect.value);
+  toEl.textContent = rawAmount;
+}
+
+const optionsHTML = generateOptions(currencies);
+
+fromSelect.innerHTML = optionsHTML;
+toSelect.innerHTML = optionsHTML;
+
+form.addEventListener('input', handleInput);
